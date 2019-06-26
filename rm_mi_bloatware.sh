62ddcd07950a67ddfa9e5138a6532e4f08c75d8c
Ramachandra Junior <ramachandrajr3@gmail.com>
2019-06-26 16:42:01 +0530

    Create rm_mi_bloatware.sh

---
+++ rm_mi_bloatware.sh
@@ -0,0 +1,93 @@
+# Mi Picks
+echo "Uninstalling xiaomi.mipicks ..."
+adb -d shell pm uninstall -k --user 0 com.xiaomi.mipicks
+# Browser
+echo "Uninstalling android.browser ..."
+adb -d shell pm uninstall -k --user 0 com.android.browser
+# Calculator
+echo "Uninstalling miui.calculator ..."
+adb -d shell pm uninstall -k --user 0 com.miui.calculator
+# Calendar
+echo "Uninstalling android.calendar ..."
+adb -d shell pm uninstall -k --user 0 com.android.calendar
+# Compass
+echo "Uninstalling miui.compass ..."
+adb -d shell pm uninstall -k --user 0 com.miui.compass
+# Feedback
+echo "Uninstalling miui.bugreport ..."
+adb -d shell pm uninstall -k --user 0 com.miui.bugreport
+# File Explorer
+echo "Uninstalling mi.android.globalFileexplorer ..."
+adb -d shell pm uninstall -k --user 0 com.mi.android.globalFileexplorer
+# FM Radio
+echo "Uninstalling miui.fm ..."
+adb -d shell pm uninstall -k --user 0 com.miui.fm
+# Gallery
+echo "Uninstalling miui.gallery ..."
+adb -d shell pm uninstall -k --user 0 com.miui.gallery
+# Games
+echo "Uninstalling xiaomi.glgm ..."
+adb -d shell pm uninstall -k --user 0 com.xiaomi.glgm
+# Mi Drop
+echo "Uninstalling xiaomi.midrop ..."
+adb -d shell pm uninstall -k --user 0 com.xiaomi.midrop
+# Mi Pay
+echo "Uninstalling mipay.wallet.id ..."
+adb -d shell pm uninstall -k --user 0 com.mipay.wallet.id
+echo "Uninstalling mipay.wallet.in ..."
+adb -d shell pm uninstall -k --user 0 com.mipay.wallet.in
+# Mi Payment
+echo "Uninstalling xiaomi.payment ..."
+adb -d shell pm uninstall -k --user 0 com.xiaomi.payment
+# Mi Recycle
+echo "Uninstalling xiaomi.mirecycle ..."
+adb -d shell pm uninstall -k --user 0 com.xiaomi.mirecycle
+# Mi Roaming
+echo "Uninstalling miui.virtualsim ..."
+adb -d shell pm uninstall -k --user 0 com.miui.virtualsim
+# Mi Video
+echo "Uninstalling miui.videoplayer ..."
+adb -d shell pm uninstall -k --user 0 com.miui.videoplayer
+# Music
+echo "Uninstalling miui.player ..."
+adb -d shell pm uninstall -k --user 0 com.miui.player
+# Notes
+echo "Uninstalling miui.notes ..."
+adb -d shell pm uninstall -k --user 0 com.miui.notes
+# Recorder
+echo "Uninstalling android.soundrecorder ..."
+adb -d shell pm uninstall -k --user 0 com.android.soundrecorder
+# Scanner
+echo "Uninstalling xiaomi.scanner ..."
+adb -d shell pm uninstall -k --user 0 com.xiaomi.scanner
+# Screen Recorder
+echo "Uninstalling miui.screenrecorder ..."
+adb -d shell pm uninstall -k --user 0 com.miui.screenrecorder
+# Security
+echo "Uninstalling miui.cleanmaster ..."
+adb -d shell pm uninstall -k --user 0 com.miui.cleanmaster
+echo "Uninstalling miui.securitycenter ..."
+adb -d shell pm uninstall -k --user 0 com.miui.securitycenter
+echo "Uninstalling miui.securityadd ..."
+adb -d shell pm uninstall -k --user 0 com.miui.securityadd
+# Wallpaper
+echo "Uninstalling miui.miwallpaper ..."
+adb -d shell pm uninstall -k --user 0 com.miui.miwallpaper
+# Weather
+echo "Uninstalling miui.weather2 ..."
+adb -d shell pm uninstall -k --user 0 com.miui.weather2
+echo "Uninstalling miui.providers.weather ..."
+adb -d shell pm uninstall -k --user 0 com.miui.providers.weather
+# Yellow Pages
+echo "Uninstalling miui.yellowpage ..."
+adb -d shell pm uninstall -k --user 0 com.miui.yellowpage
+# AntiSpam
+echo "Uninstalling miui.antispam ..."
+adb -d shell pm uninstall -k --user 0 com.miui.antispam
+# SIM Activate Services
+echo "Uninstalling xiaomi.simactivate.service ..."
+adb -d shell pm uninstall -k --user 0 com.xiaomi.simactivate.service
+# MSA
+echo "Uninstalling miui.msa.global ..."
+adb -d shell pm uninstall -k --user 0 com.miui.msa.global
+
